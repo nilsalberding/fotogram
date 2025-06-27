704c46b54422b46bc53826634f66594c8f7857cd
Nils Alberding-Schulz <nilsalberding@web.de>
2025-06-27 10:24:48 +0200

    added more objects to array, added some details in css

--- script.js
+++ script.js
@@ -3,6 +3,71 @@ const imagesBread = [
         name: "Brot 1",
         path: "./img/bread1.jpg",
         alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 2",
+        path: "./img/bread2.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 3",
+        path: "./img/bread3.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 4",
+        path: "./img/bread4.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 5",
+        path: "./img/bread5.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 6",
+        path: "./img/bread6.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 7",
+        path: "./img/bread7.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 8",
+        path: "./img/bread8.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 9",
+        path: "./img/bread9.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 10",
+        path: "./img/bread10.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 11",
+        path: "./img/bread11.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 12",
+        path: "./img/bread12.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 13",
+        path: "./img/bread13.jpg",
+        alt: "Ein herrliches Brot"
+    },
+        {
+        name: "Brot 14",
+        path: "./img/bread14.jpg",
+        alt: "Ein herrliches Brot"
     }
 ]
 
